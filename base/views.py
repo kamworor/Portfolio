31f439761db21f8418b9445b83959bc870ec5de8
kamworor <51693048+kamworor@users.noreply.github.com>
2023-10-27 10:27:50 +0300

    first com

---
+++ base/views.py
@@ -0,0 +1,115 @@
+from django.shortcuts import render, redirect   
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from django.core.mail import EmailMessage
+from django.core.mail.backends import smtp
+
+from django.core.mail import send_mail
+from django.conf import settings
+from django.template.loader import render_to_string
+from .models import Post
+from .forms import PostForm
+from .filters import PostFilter
+# Create your views here.
+
+def home(request): 
+    posts = Post.objects.filter(active=True, featured=True)[0:3]
+   
+
+
+    context = {'posts':posts}
+    return render(request, 'base/index.html', context) 
+
+def post(request, slug):
+    post = Post.objects.get(slug=slug)  
+     
+    context = {'post':post}
+    return render(request, 'base/post.html', context)
+
+def posts(request):
+    posts = Post.objects.filter(active=True)
+    myFilter = PostFilter(request.GET, queryset=posts)
+    filtered_posts = myFilter.qs
+
+    ordered_posts = filtered_posts.order_by('-created')
+
+
+    paginator = Paginator(ordered_posts, 2)
+
+    page = request.GET.get('page')
+
+    try:
+        paginated_posts = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_posts = paginator.page(1)
+    except EmptyPage:
+        paginated_posts = paginator.page(paginator.num_pages)
+    
+
+    context = {'posts':paginated_posts,'myFilter':myFilter}
+    return render(request, 'base/posts.html', context)
+ 
+def profile(request):
+    return render(request, 'base/profile.html') 
+ 
+@login_required(login_url="home")
+def createPost(request):
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('posts')
+
+    context = {'form':form}
+    return render(request, 'base/post_form.html', context)
+
+@login_required(login_url="home")
+def updatePost(request, slug):
+    post = Post.objects.get(slug=slug)
+    form = PostForm(instance=post)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+        return redirect('posts')
+
+    context = {'form':form}
+    return render(request, 'base/post_form.html', context)
+
+
+@login_required(login_url="home")
+def deletePost(request,slug):
+    post = Post.objects.get(slug=slug)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('posts')
+    context = {'item':post}
+    return render(request, 'base/delete.html', context)
+
+
+def sendEmail(request):
+
+    if request.method == 'POST':
+
+        template = render_to_string('base/email_template.html',{
+            'name': request.POST['name'],
+            'email': request.POST['email'],
+            'message': request.POST['message'],
+        })
+        email = EmailMessage(
+            request.POST['subject'],
+            template,
+            settings.EMAIL_HOST_USER,
+            ['stevekamworor@gmail.com']
+        )
+        email.fail_silently=False
+        email.send()
+
+    return render(request, 'base/email_sent.html')
+ 
